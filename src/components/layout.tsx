bf4797bf8a1c26c7c5c5955e1c7acbd95887ada5
Yusuf <yusufhameed911@gmail.com>
2025-06-22 13:43:23 +0530

    fix app

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -12,7 +12,7 @@ export function Layout({ children }: PropsWithChildren) {
       <footer className="border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-12">
         <a
           target="_blank"
-          href="https://github.com/Nexonikk"
+          href="https://github.com/Nexonikk/climate.app"
           className="flex overflow-hidden items-center mx-auto text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-black text-white shadow hover:bg-black/90 h-9 px-4 py-2 max-w-52 whitespace-pre md:flex group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out hover:ring-2 hover:ring-black hover:ring-offset-2"
         >
           <span className="absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 bg-white opacity-10 transition-all duration-1000 ease-out group-hover:-translate-x-40"></span>
